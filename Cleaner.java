76af1d1d6023294d7f6a797a7af7dac6ee5cd70b
Sileniful <darkk19@gmail.com>
2014-11-05 12:34:58 +0100

    v1.0
    new file

---
+++ Cleaner.java
@@ -0,0 +1,15 @@
+package Users;
+
+public class Cleaner extends Employee {
+	
+	public Cleaner(){
+		
+	}
+	
+	public Cleaner(String name, String surname, String address, int ID, int dateOfBirth, int payRoleID, int salary){
+
+		super(name, surname, address, ID, dateOfBirth, payRoleID, salary);
+
+	}
+
+}
