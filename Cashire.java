cc6171d374af0f64a60c70f75e726cf562caf48e
Sileniful <darkk19@gmail.com>
2014-11-05 12:34:32 +0100

    v1.1
    added constructor

--- Cashire.java
+++ Cashire.java
@@ -1,14 +1,16 @@
 package Users;
 
+
 public class Cashier extends Employee {
+
 	
 	public Cashier(){
-		
-		
+
 	}
 
 	public Cashier(String name, String surname, String address, int ID, int dateOfBirth, int payRoleID, int Salary){
+
 		super(name, surname, address, ID, dateOfBirth, payRoleID, Salary);
-		
+
 	}
 }
