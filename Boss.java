c1b8d020eda3f7882d3a6b741fb6f8655e349ee9
Sileniful <darkk19@gmail.com>
2014-11-05 12:32:10 +0100

    new file

---
+++ Boss.java
@@ -0,0 +1,14 @@
+package Users;
+
+public class Boss extends Manager {
+	
+	public Boss(){
+		
+	}
+	
+	public Boss(String name, String surname, String address, int ID, int dateOfBirth, int payRoleID, int salary){
+		super(name, surname, address, ID, dateOfBirth, payRoleID, salary);
+		
+	}
+	
+}
